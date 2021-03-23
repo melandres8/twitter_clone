70d9f8ef25d73cc8b30dc16b9693c1b8e1f0b4ca
Melkin Mosquera <melanmosta@gmail.com>
2021-03-23 14:52:32 -0500

    making pagination functional for follow system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,14 @@
 class UsersController < ApplicationController
-  before_action :set_users, only: [:followers, :following]
-
   def show
     @user = User.find(params[:id])
     @tweets = Tweet.all.order("created_at DESC").paginate(page: params[:page], per_page: 10)
   end
 
   def followers
+    @user_followers = current_user.followers.order("name ASC").paginate(page: params[:page], per_page: 10)
   end
 
   def following
-  end
-
-  private
-
-  def set_users
-    @users = User.all.order("name ASC").paginate(page: params[:page], per_page: 10)
+    @user_following = User.all.order("name ASC").paginate(page: params[:page], per_page: 4)
   end
 end
